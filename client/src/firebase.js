70b2cbf71a1d43f42f88648695d88d9222fcd2a2
Gourav Agarwal <92258067+Gouravag11@users.noreply.github.com>
2024-08-17 23:29:52 +0530

    Important

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVvt0zCIgMj48OWb6W1ENej7zNCi7FoRM",
-  authDomain: "aura-gaming11.firebaseapp.com",
-  projectId: "aura-gaming11",
-  storageBucket: "aura-gaming11.appspot.com",
-  messagingSenderId: "418642788373",
-  appId: "1:418642788373:web:bb0294382570d245b70e1c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-69ZPPJWZM0"
 };
 
