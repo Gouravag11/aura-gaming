a920822c9147d4d418f24e2d61cf76e448b3475a
Gourav Agarwal <92258067+Gouravag11@users.noreply.github.com>
2024-08-31 11:11:24 +0530

    minor

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
-const eventRoutes = require('./routes/events'); // Ensure this path is correct
+const eventRoutes = require('./routes/events');
 require('dotenv').config();
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,27 +18,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/events', eventRoutes); // Ensure this line is present
+app.use('/api/events', eventRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Aura Gaming Backend!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/public')));
-if (process.env.NODE_ENV === 'production') {
-// Serve the static files from the React app
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-// Catch-all handler to serve React's index.html for any unknown routes
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
-// });
